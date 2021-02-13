fc8b28dd00482165985ec32bd59d4884b3c42b0d
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-12 16:17:15 -0800

    Attempt to implement sessionStorage for the API call, storage,stringify,and parse are all working, just need to get the display and math proper

--- src/main.js
+++ src/main.js
@@ -5,6 +5,24 @@ import './css/styles.css';
 import ExService from './js/exchange-service';
 import calcRate from './js/calc';
 
+async function listPop() {
+  let code = "USD";
+  let exList = await ExService.currencyCall(code);
+  if (exList.result === "success") {
+    for (let i = 0; i < Object.keys(exList.conversion_rates).length; i++) {
+      $("#exRateOne").append(`<option value= "${Object.keys(exList.conversion_rates)[i]}"> ${Object.keys(exList.conversion_rates)[i]}</option>`);
+      $("#exRateTwo").append(`<option value= "${Object.keys(exList.conversion_rates)[i]}"> ${Object.keys(exList.conversion_rates)[i]}</option>`);
+    }
+    sessionStorage.setItem("exchangeRatesUSD", JSON.stringify(exList.conversion_rates));
+    console.log(sessionStorage);
+  } else {
+    let wtf = "error-type";
+    $('.errorMessage').append(`The page did not populate the lists correctly.<br>
+    There was an error: ${exList[wtf]}.`);
+    $('.errorMessage').show();
+  }
+}
+
 async function currConv() {
   let exValue = 0;
   let input = $("input").val();
@@ -14,18 +32,22 @@ async function currConv() {
   }
   let EX1 = $("#exRateOne").val();
   let EX2 = $("#exRateTwo").val();
-  let exArray = await ExService.currencyCall(EX1);
+  let exArray = JSON.parse(sessionStorage.getItem("exchangeRatesUSD"));
+  // let exArray = await ExService.currencyCall(EX1);
   console.log(exArray);
-  if (exArray.result) {
-    if (exArray.conversion_rates[EX1] && exArray.conversion_rates[EX2]) {
-      exValue = calcRate(input, exArray, EX2);
-      $('.results').append(`You would have ${exValue} ${EX2} after the exchange.`);
-      $('.results').show();
-    } else {
-      $('.errorMessage').append(`The exchange did not go through.<br>
-      There was an error: Codes did not match data`);
-      $('.errorMessage').show();
-    }
+  if (exArray[0]) {
+    exValue = calcRate(input, exArray, EX1, EX2);
+    $('.results').append(`You would have ${exValue} ${EX2} after the exchange.`);
+
+    // if (exArray.conversion_rates[EX1] && exArray.conversion_rates[EX2]) {
+    //   exValue = calcRate(input, exArray, EX2);
+    //   $('.results').append(`You would have ${exValue} ${EX2} after the exchange.`);
+    //   $('.results').show();
+    // } else {
+    //   $('.errorMessage').append(`The exchange did not go through.<br>
+    //   There was an error: Codes did not match data`);
+    //   $('.errorMessage').show();
+    // }
   } else {
     $('.errorMessage').append(`The exchange did not go through.<br>
     There was an error: ${exArray.message}`);
@@ -33,21 +55,6 @@ async function currConv() {
   }
 }
 
-async function listPop() {
-  let code = "USD";
-  let exList = await ExService.currencyCall(code);
-  if (exList.result === "success") {
-    for (let i = 0; i < Object.keys(exList.conversion_rates).length; i++) {
-      $("#exRateOne").append(`<option value= "${Object.keys(exList.conversion_rates)[i]}"> ${Object.keys(exList.conversion_rates)[i]}</option>`);
-      $("#exRateTwo").append(`<option value= "${Object.keys(exList.conversion_rates)[i]}"> ${Object.keys(exList.conversion_rates)[i]}</option>`);
-    }
-  } else {
-    let wtf = "error-type";
-    $('.errorMessage').append(`The page did not populate the lists correctly.<br>
-    There was an error: ${exList[wtf]}.`);
-    $('.errorMessage').show();
-  }
-}
 
 window.onload = function () {
   listPop();
